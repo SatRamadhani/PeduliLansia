521eb208acba12430370af08b9b047e5d27331ec
SatRamadhani <satriaramadhani1011@gmail.com>
2022-10-15 15:51:12 +0700

    Activity test.

---
+++ app/src/main/java/edu/upi/cs/mobileapp/techi/pedulilansia/MainActivity.java
@@ -0,0 +1,15 @@
+package edu.upi.cs.mobileapp.techi.pedulilansia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
