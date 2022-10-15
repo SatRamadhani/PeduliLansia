9386f0f20de10e8b0e1d49c14024535e02f71a7c
Satria Pinandita Abyatarsyah <satriapinanditaabyatarsyah@gmail.com>
2022-10-15 17:08:21 +0700

    Update SplashScreenActivity.

--- app/src/main/java/edu/upi/cs/mobileapp/techi/pedulilansia/SplashScreenActivity.java
+++ app/src/main/java/edu/upi/cs/mobileapp/techi/pedulilansia/SplashScreenActivity.java
@@ -2,13 +2,16 @@ package edu.upi.cs.mobileapp.techi.pedulilansia;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_screen);
+        startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
+        finish();
     }
 }
