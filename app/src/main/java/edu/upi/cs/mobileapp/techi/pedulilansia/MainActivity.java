a278b50f7a1468795043a23bfb50cfb6d897e2a2
SatRamadhani <satriaramadhani1011@gmail.com>
2022-10-15 17:44:58 +0700

    Refine SplashScreenActivity.

--- app/src/main/java/edu/upi/cs/mobileapp/techi/pedulilansia/MainActivity.java
+++ app/src/main/java/edu/upi/cs/mobileapp/techi/pedulilansia/MainActivity.java
@@ -12,4 +12,10 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    protected void onPostResume()
+    {
+        super.onPostResume();
+    }
 }
--- app/src/main/java/edu/upi/cs/mobileapp/techi/pedulilansia/SplashScreenActivity.java
+++ app/src/main/java/edu/upi/cs/mobileapp/techi/pedulilansia/SplashScreenActivity.java
@@ -4,14 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
-public class SplashScreenActivity extends AppCompatActivity {
+public class SplashScreenActivity extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-        startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
-        finish();
+
+        Handler handler = new Handler();
+        handler.postDelayed(() ->
+        {
+            startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
+            finish();
+        }, 3000);
     }
 }
