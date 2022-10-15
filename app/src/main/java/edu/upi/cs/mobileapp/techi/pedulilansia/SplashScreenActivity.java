65304e756fa2e0bd71d5e7a4bfdcb26c9a97f7fd
Satria Pinandita Abyatarsyah <satriapinanditaabyatarsyah@gmail.com>
2022-10-15 16:10:25 +0700

    Add SplashScreenActivity.

---
+++ app/src/main/java/edu/upi/cs/mobileapp/techi/pedulilansia/SplashScreenActivity.java
@@ -0,0 +1,14 @@
+package edu.upi.cs.mobileapp.techi.pedulilansia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
